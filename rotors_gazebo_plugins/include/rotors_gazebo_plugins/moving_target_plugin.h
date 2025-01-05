99b42919429b6e552b2e6b24c47d9f2fb16a0453
Orienfish <x1yu@ucsd.edu>
2025-01-05 14:18:35 -0800

    add moving target

---
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/moving_target_plugin.h
@@ -0,0 +1,47 @@
+#ifndef MOVING_TARGET_PLUGIN_H
+#define MOVING_TARGET_PLUGIN_H
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/Plugin.hh>
+#include <ignition/math/Vector3.hh>
+#include <gazebo/common/Time.hh>
+
+namespace gazebo
+{
+  class MovingTargetPlugin : public ModelPlugin
+  {
+  public:
+    /// Constructor
+    MovingTargetPlugin() = default;
+
+    /// Destructor
+    virtual ~MovingTargetPlugin() = default;
+
+    /// Load the plugin (called by Gazebo)
+    /// @param model Pointer to the parent model
+    /// @param sdf Pointer to the SDF element
+    void Load(physics::ModelPtr model, sdf::ElementPtr sdf) override;
+
+    /// Update function called at every simulation iteration
+    void OnUpdate();
+
+  private:
+    /// Pointer to the model this plugin is attached to
+    physics::ModelPtr model;
+
+    /// Pointer to the world update event connection
+    event::ConnectionPtr updateConnection;
+
+    /// Target's constant velocity vector
+    ignition::math::Vector3d velocity;
+
+    /// Last update time
+    common::Time lastUpdateTime;
+  };
+}
+
+// Macro to register the plugin with Gazebo
+// GZ_REGISTER_MODEL_PLUGIN(gazebo::MovingTargetPlugin)
+
+#endif // MOVING_TARGET_PLUGIN_H
---
+++ rotors_gazebo_plugins/src/moving_target_plugin.cpp
@@ -0,0 +1,57 @@
+#include "rotors_gazebo_plugins/moving_target_plugin.h"
+
+namespace gazebo
+{
+  // Load function: Called once when the plugin is loaded
+  void MovingTargetPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+  {
+    // Store the model pointer
+    this->model = model;
+
+    // Initialize velocity to default values or values from SDF
+    this->velocity = ignition::math::Vector3d(0, 0, 0); // Default velocity
+
+    // Check for velocity settings in SDF
+    if (sdf->HasElement("velocity"))
+    {
+      this->velocity = sdf->Get<ignition::math::Vector3d>("velocity");
+    }
+
+    // Print confirmation to console
+    gzdbg << "MovingTargetPlugin loaded with velocity: " 
+          << this->velocity << std::endl;
+
+    // Connect to the world update event
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+        std::bind(&MovingTargetPlugin::OnUpdate, this));
+  }
+
+  // OnUpdate function: Called every simulation iteration
+  void MovingTargetPlugin::OnUpdate()
+  {
+    // Get the current simulation time
+    common::Time currentTime = this->model->GetWorld()->SimTime();
+
+    // Calculate the time delta
+    double timeDelta = (currentTime - this->lastUpdateTime).Double();
+
+    // Update the target's position
+    if (timeDelta > 0)
+    {
+      // Get the current position
+      ignition::math::Pose3d currentPose = this->model->WorldPose();
+
+      // Calculate the new position
+      ignition::math::Vector3d newPosition = currentPose.Pos() + this->velocity * timeDelta;
+
+      // Set the new position
+      this->model->SetWorldPose(ignition::math::Pose3d(newPosition, currentPose.Rot()));
+
+      // Update the last update time
+      this->lastUpdateTime = currentTime;
+    }
+  }
+
+// Register the plugin with Gazebo
+GZ_REGISTER_MODEL_PLUGIN(MovingTargetPlugin);
+}
