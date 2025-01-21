491ad62eaf10096c2e7316163ac610800e88f2fb
Orienfish <x1yu@ucsd.edu>
2025-01-20 22:08:34 -0800

    add camera logger

--- rotors_gazebo/scripts/my_logger.py
+++ rotors_gazebo/scripts/my_logger.py
@@ -2,15 +2,21 @@
 
 import rospy
 import csv
-from geometry_msgs.msg import Point
+from sensor_msgs.msg import Image
 from nav_msgs.msg import Odometry
 from gazebo_msgs.msg import ModelStates
 from tf.transformations import euler_from_quaternion
+from cv_bridge import CvBridge
+import cv2
+import os
 
 # File for logging
 log_file_odom = "./drone_log.csv"
 log_file_ground_truth = "./drone_ground_truth.csv"
 log_file_target = "./target_log.csv"
+log_image_dir = "./drone_images"
+
+last_save_time = None
 
 def init_csv_odom_file(filename):
     # Initialize CSV file
@@ -63,8 +69,38 @@ def car_position_callback(msg, log_filename):
         writer = csv.writer(file)
         writer.writerow([rospy.get_time(), x, y, z])
 
+def image_callback(msg):
+    global last_save_time
+
+    # Check if 1 second has passed since the last save
+    if rospy.get_time() - last_save_time >= 1:
+        # Initialize the CvBridge
+        bridge = CvBridge()
+
+        try:
+            # Convert the ROS image message to an OpenCV image
+            cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+
+            # Get the current timestamp for unique filenames
+            filename = os.path.join(log_image_dir, "{:.6f}.png".format(rospy.get_time()))
+
+            # Save the image
+            cv2.imwrite(filename, cv_image)
+
+            rospy.loginfo("Saved image to {}".format(filename))
+
+            # Update the last save time
+            last_save_time = rospy.get_time()
+
+        except Exception as e:
+            rospy.logerr("Failed to convert image: {}".format(e))
+
 def main():
+    global last_save_time
     rospy.init_node("my_logger", anonymous=True)
+    last_save_time = rospy.get_time()
+
+    # Create the csv files for odom and pos logging
     init_csv_odom_file(log_file_odom)
     init_csv_odom_file(log_file_ground_truth)
     init_csv_position_file(log_file_target)
@@ -73,6 +109,8 @@ def main():
     rospy.Subscriber("/hummingbird/odometry_sensor1/odometry", Odometry, lambda msg: odom_callback(msg, log_file_odom))
     rospy.Subscriber("/hummingbird/ground_truth/odometry", Odometry, lambda msg: odom_callback(msg, log_file_ground_truth))
     rospy.Subscriber('/gazebo/model_states', ModelStates, lambda msg: car_position_callback(msg, log_file_target))
+    rospy.Subscriber("/hummingbird/camera_nadir/image_raw", Image, image_callback)
+
 
     # Keep node running
     rospy.spin()
